37180d94e3501bc63abf0e3145ea58183b23522e
Naodsh <naodadanech1977@gmail.com>
2024-06-12 15:11:02 +0300

    2-c_route.py

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+simple Flask web application that displays 'Hello HBNB!',
+ 'HBNB', and 'C <text>' at respective routes.
+"""
+
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+ @app.route('/', strict_slashes=False)
+ def hello_hbnb():
+     """
+     Route handler that returns the string 'Hello HBNB!'
+     """
+
+     return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+    Route handler that returns the string 'HBNB'
+    """
+
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """
+    Route handler returns 'C ' followed by the value of the text variable.
+    Replaces underscores in the text with spaces.
+    """
+
+    return "C " + text.replace('_', ' ')
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)        
