5434fe13e28767dcae478b42338898ae081562a6
Naodsh <naodadanech1977@gmail.com>
2024-06-12 15:16:25 +0300

    2-c_route.py

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
 simple Flask web application that displays 'Hello HBNB!',
- 'HBNB', and 'C <text>' at respective routes.
+'HBNB', and 'C <text>' at respective routes.
 """
 
 
@@ -10,13 +10,13 @@ from flask import Flask
 app = Flask(__name__)
 
 
- @app.route('/', strict_slashes=False)
- def hello_hbnb():
-     """
-     Route handler that returns the string 'Hello HBNB!'
-     """
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """
+    Route handler that returns the string 'Hello HBNB!'
+    """
 
-     return "Hello HBNB!"
+    return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
@@ -39,4 +39,4 @@ def c_text(text):
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)        
+    app.run(host="0.0.0.0", port=5000)
